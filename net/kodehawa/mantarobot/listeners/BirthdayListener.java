296fae41b2ca76874ffbca4c7fe6d4340162d60b
Kodehawa <david.alejandro.rubio@gmail.com>
2016-12-23 19:14:14 -0300

    Another attempt to fix birthday pls

--- net/kodehawa/mantarobot/listeners/BirthdayListener.java
+++ net/kodehawa/mantarobot/listeners/BirthdayListener.java
@@ -8,6 +8,7 @@ import net.kodehawa.mantarobot.cmd.Birthday;
 import net.kodehawa.mantarobot.cmd.guild.Parameters;
 import net.kodehawa.mantarobot.log.LogType;
 import net.kodehawa.mantarobot.log.Logger;
+import net.kodehawa.mantarobot.thread.AsyncHelper;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -16,37 +17,36 @@ import java.util.List;
 public class BirthdayListener extends ListenerAdapter {
     private Guild guild;
     private MessageChannel channel;
-    private Member userToAssign;
+    private Member membertoAssign;
 
     public BirthdayListener(){
         Logger.instance().print("Birthday Logger started.", this.getClass(), LogType.INFO);
     }
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        guild = event.getGuild();
-        channel = event.getChannel();
-        if(Parameters.getBirthdayHash().containsKey(guild.getId())) {
-            String userKey = event.getGuild().getId() + ":" + event.getAuthor().getId();
-            String[] data = userKey.split(":");
-            if(Birthday.bd.containsKey(userKey)){
-                if(!Birthday.bd.get(userKey).isEmpty()){
-                    try{
-                        Calendar cal = Calendar.getInstance();
-                        SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
-                        if(Birthday.bd.get(userKey).substring(0, 5).equals(format1.format(cal.getTime()).substring(0, 5))) {
-                            String guildId = data[0];
-                            List<Member> user = event.getGuild().getMembers();
-                            Role birthdayRole =
-                                    guild.getRoleById(Parameters.getBirthdayRoleForServer(guildId));
-                            for (Member finalMember : user) {
-                                if (finalMember.getUser().getId().equals(event.getAuthor().getId())) {
-                                    userToAssign = finalMember;
-                                    if (!guild.getMembersWithRoles(birthdayRole).contains(finalMember)) {
-                                        guild.getController().addRolesToMember(finalMember, birthdayRole).queue(
+        Runnable r = () ->
+        {
+            guild = event.getGuild();
+            channel = event.getChannel();
+            if(Parameters.getBirthdayHash().containsKey(guild.getId())) {
+                String userKey = event.getGuild().getId() + ":" + event.getAuthor().getId();
+                String[] data = userKey.split(":");
+                if(Birthday.bd.containsKey(userKey)){
+                    if(!Birthday.bd.get(userKey).isEmpty()){
+                        try{
+                            Calendar cal = Calendar.getInstance();
+                            SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
+                            if(Birthday.bd.get(userKey).substring(0, 5).equals(format1.format(cal.getTime()).substring(0, 5))) {
+                                String guildId = data[0];
+                                Role birthdayRole = guild.getRoleById(Parameters.getBirthdayRoleForServer(guildId));
+                                if (data[1].equals(event.getAuthor().getId())) {
+                                    membertoAssign = event.getGuild().getMember(event.getAuthor());
+                                    if (!membertoAssign.getRoles().contains(birthdayRole)) {
+                                        guild.getController().addRolesToMember(membertoAssign, birthdayRole).queue(
                                                 success -> {
-                                                    TextChannel tc = guild.getTextChannelById(Parameters.getBirthdayChannelForServer(guildId));
-                                                    tc.sendMessage(":tada: **" + userToAssign.getEffectiveName() +
-                                                            " is a year older now! Wish them a happy birthday.** :tada:").queue();
+                                                        TextChannel tc = guild.getTextChannelById(Parameters.getBirthdayChannelForServer(guildId));
+                                                        tc.sendMessage(":tada: **" + membertoAssign.getEffectiveName() +
+                                                                " is a year older now! Wish them a happy birthday.** :tada:").queue();
                                                 },
                                                 error ->{
                                                     if(error instanceof PermissionException){
@@ -60,23 +60,24 @@ public class BirthdayListener extends ListenerAdapter {
                                                 });
                                     }
                                 }
-                            }
-                        } else {
-                            String guildId = data[0];
-                            Role birthdayRole = guild.getRoleById(Parameters.getBirthdayRoleForServer(guildId));
-                            List<Member> user = event.getGuild().getMembers();
-                            for (Member finalMember : user) {
-                                if(guild.getMembersWithRoles(birthdayRole).contains(finalMember)){
-                                    guild.getController().removeRolesFromMember(finalMember, birthdayRole).queue();
+                            } else {
+                                String guildId1 = data[0];
+                                Role birthdayRole1 = guild.getRoleById(Parameters.getBirthdayRoleForServer(guildId1));
+                                List<Member> user1 = event.getGuild().getMembers();
+                                for (Member finalMember1 : user1) {
+                                    if(guild.getMembersWithRoles(birthdayRole1).contains(finalMember1)){
+                                        guild.getController().removeRolesFromMember(finalMember1, birthdayRole1).queue();
+                                    }
                                 }
                             }
+                        } catch(Exception e){
+                            Logger.instance().print("Cannot process birthday for: " + userKey + " program will be still running.", this.getClass(), LogType.WARNING);
+                            e.printStackTrace();
                         }
-                    } catch(Exception e){
-                        Logger.instance().print("Cannot process birthday for: " + userKey + " program will be still running.", this.getClass(), LogType.WARNING);
-                        e.printStackTrace();
                     }
                 }
             }
-        }
+        };
+        AsyncHelper.instance().asyncThread("Birthday Listener", r).run();
     }
 }
