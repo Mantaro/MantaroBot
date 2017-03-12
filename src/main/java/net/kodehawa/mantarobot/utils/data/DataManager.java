827f971fdae17ac784f4947237cdf869f48ed0a7
Natan <semnomemonmes@gmail.com>
2017-03-12 18:50:11 -0300

    Oops

--- src/main/java/net/kodehawa/mantarobot/utils/data/DataManager.java
+++ src/main/java/net/kodehawa/mantarobot/utils/data/DataManager.java
@@ -1,11 +1,7 @@
 package net.kodehawa.mantarobot.utils.data;
 
-import java.io.Closeable;
 import java.util.function.Supplier;
 
-public interface DataManager<T> extends Supplier<T>, Closeable {
+public interface DataManager<T> extends Supplier<T> {
 	void save();
-	default void close() {
-	    save();
-    }
 }
