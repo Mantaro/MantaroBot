48fd04ba7719ad52482a481d38021e3c35afec67
Kodehawa <david.alejandro.rubio@gmail.com>
2017-03-19 23:33:15 -0300

    placeholder

--- src/main/java/net/kodehawa/mantarobot/core/listeners/MantaroListener.java
+++ src/main/java/net/kodehawa/mantarobot/core/listeners/MantaroListener.java
@@ -129,6 +129,7 @@ public class MantaroListener implements EventListener {
 	}
 
 	private void logDelete(GuildMessageDeleteEvent event) {
+		if(event.getJDA().getSelfUser().getId().equals(event.getAuthor().getId())) return;
 		try {
 			String hour = df.format(new Date(System.currentTimeMillis()));
 			String logChannel = MantaroData.getData().get().getGuild(event.getGuild(), false).logChannel;
