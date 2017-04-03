b5e98e7586ca186f788de7e4524600776af0bd58
StupPlayer <stupplayer@outlook.com>
2017-04-02 21:14:43 -0300

    Query Command

--- src/main/java/net/kodehawa/mantarobot/commands/OwnerCmd.java
+++ src/main/java/net/kodehawa/mantarobot/commands/OwnerCmd.java
@@ -24,7 +24,9 @@ import net.kodehawa.mantarobot.modules.Category;
 import net.kodehawa.mantarobot.modules.CommandPermission;
 import net.kodehawa.mantarobot.modules.Module;
 import net.kodehawa.mantarobot.modules.SimpleCommand;
+import net.kodehawa.mantarobot.utils.Utils;
 import net.kodehawa.mantarobot.utils.commands.EmoteReference;
+import net.kodehawa.mantarobot.utils.sql.SQLDatabase;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,6 +34,11 @@ import org.slf4j.LoggerFactory;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.awt.Color;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -484,6 +491,49 @@ public class OwnerCmd extends Module {
 					return;
 				}
 
+				if (option.equals("query")) {
+					try {
+						SQLDatabase.getInstance().run((conn) -> {
+							try {
+								ResultSet set;
+								try {
+									set = conn.prepareStatement(v).executeQuery();
+								} catch (SQLException e) {
+									try {
+										conn.prepareStatement(v).execute();
+										event.getChannel().sendMessage(" Query was successfully executed!").queue();
+									} catch (SQLException e1) {
+										event.getChannel().sendMessage("Failed to execute query! " + Utils.paste(getStackTrace(e1))).queue();
+									}
+									return;
+								}
+								List<String> header = new ArrayList<>();
+								List<List<String>> table = new ArrayList<>();
+								ResultSetMetaData metaData = set.getMetaData();
+								int columnsCount = metaData.getColumnCount();
+								for (int i = 0; i < columnsCount; i++) {
+									header.add(metaData.getColumnName(i + 1));
+								}
+								while (set.next()) {
+									List<String> row = new ArrayList<>();
+									for (int i = 0; i < columnsCount; i++) {
+										String s = String.valueOf(set.getString(i + 1)).trim();
+										row.add(s.substring(0, Math.min(30, s.length())));
+									}
+									table.add(row);
+								}
+								String output = makeAsciiTable(header, table, null);
+								event.getChannel().sendMessage(output).queue();
+							} catch (SQLException e) {
+								event.getChannel().sendMessage(" Failed to build ascii table! " + Utils.paste(getStackTrace(e))).queue();
+							}
+						}).queue();
+					} catch (SQLException e) {
+						event.getChannel().sendMessage(" Failed to run query! " + Utils.paste(getStackTrace(e))).queue();
+					}
+					return;
+				}
+
 				onHelp(event);
 			}
 
@@ -527,5 +577,70 @@ public class OwnerCmd extends Module {
 		event.getChannel().sendMessage(random(sleepQuotes)).complete();
 
 		Arrays.stream(MantaroBot.getInstance().getShards()).forEach(mantaroShard -> mantaroShard.getJDA().shutdown(true));
+	}
+
+	public static String makeAsciiTable(List<String> headers, List<List<String>> table, List<String> footer) {
+		StringBuilder sb = new StringBuilder();
+		int padding = 1;
+		int[] widths = new int[headers.size()];
+		for (int i = 0; i < widths.length; i++) {
+			widths[i] = 0;
+		}
+		for (int i = 0; i < headers.size(); i++) {
+			if (headers.get(i).length() > widths[i]) {
+				widths[i] = headers.get(i).length();
+				if (footer != null) {
+					widths[i] = Math.max(widths[i], footer.get(i).length());
+				}
+			}
+		}
+		for (List<String> row : table) {
+			for (int i = 0; i < row.size(); i++) {
+				String cell = row.get(i);
+				if (cell.length() > widths[i]) {
+					widths[i] = cell.length();
+				}
+			}
+		}
+		sb.append("```").append("\n");
+		String formatLine = "|";
+		for (int width : widths) {
+			formatLine += " %-" + width + "s |";
+		}
+		formatLine += "\n";
+		sb.append(appendSeparatorLine("+", "+", "+", padding, widths));
+		sb.append(String.format(formatLine, headers.toArray()));
+		sb.append(appendSeparatorLine("+", "+", "+", padding, widths));
+		for (List<String> row : table) {
+			sb.append(String.format(formatLine, row.toArray()));
+		}
+		if (footer != null) {
+			sb.append(appendSeparatorLine("+", "+", "+", padding, widths));
+			sb.append(String.format(formatLine, footer.toArray()));
+		}
+		sb.append(appendSeparatorLine("+", "+", "+", padding, widths));
+		sb.append("```");
+		return sb.toString();
+	}
+
+	private static String appendSeparatorLine(String left, String middle, String right, int padding, int... sizes) {
+		boolean first = true;
+		StringBuilder ret = new StringBuilder();
+		for (int size : sizes) {
+			if (first) {
+				first = false;
+				ret.append(left).append(String.join("", Collections.nCopies(size + padding * 2, "-")));
+			} else {
+				ret.append(middle).append(String.join("", Collections.nCopies(size + padding * 2, "-")));
+			}
+		}
+		return ret.append(right).append("\n").toString();
+	}
+
+	public static String getStackTrace(Throwable e){
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		e.printStackTrace(pw);
+		return sw.toString();
 	}
 }
