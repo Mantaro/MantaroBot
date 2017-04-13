6e8be0862821951d8dd7e99d8008cd2a3342ee77
Adam <adrat205@gmail.com>
2017-04-13 16:39:28 -0400

    ~>ass

--- src/main/java/net/kodehawa/mantarobot/commands/FunCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/FunCmds.java
@@ -8,7 +8,14 @@ import net.kodehawa.mantarobot.modules.CommandPermission;
 import net.kodehawa.mantarobot.modules.Module;
 import net.kodehawa.mantarobot.modules.SimpleCommand;
 import net.kodehawa.mantarobot.utils.commands.EmoteReference;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class FunCmds extends Module {
@@ -17,6 +24,37 @@ public class FunCmds extends Module {
         super(Category.FUN);
         coinflip();
         dice();
+        ass();
+    }
+
+    private void ass() {
+        super.register("ass", new SimpleCommand() {
+            @Override
+            protected void call(String[] args, String content, GuildMessageReceivedEvent event) {
+                try {
+                    Document assList = Jsoup.parse(new URL("http://thechive.com/2017/04/11/im-in-the-business-of-booty-scoops-and-business-is-a-boomin-33" +
+                                "-photos/"), 10000);
+                    Elements links = assList.getElementsByTag("<img>");
+                    ArrayList<String> asses = new ArrayList();
+                    links.forEach(img -> {
+                        String url = img.attr("src");
+                        if (url.contains(".jpeg")) asses.add(url);
+                    });
+                    event.getChannel().sendMessage(asses.get(new SecureRandom().nextInt(asses.size()))).queue();
+                }
+                catch (java.io.IOException e) {
+                    event.getChannel().sendMessage("Fuck, something broke").queue();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public MessageEmbed help(GuildMessageReceivedEvent event) {
+                return helpEmbed(event, "See some hot asses")
+                        .setDescription("wew lad")
+                        .build();
+            }
+        });
     }
 
     private void coinflip() {
