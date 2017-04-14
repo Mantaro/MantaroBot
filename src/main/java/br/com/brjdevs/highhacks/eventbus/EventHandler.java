6cda622178f5e9340c4c2b5a2bcf3279b2754478
Natan <semnomemonmes@gmail.com>
2017-04-14 19:05:20 -0300

    [Mantaro] Adding these files while the lib is not on bintray

---
+++ src/main/java/br/com/brjdevs/highhacks/eventbus/EventHandler.java
@@ -0,0 +1,14 @@
+package br.com.brjdevs.highhacks.eventbus;
+
+/**
+ * Handles invocation of a listener
+ */
+@FunctionalInterface
+public interface EventHandler {
+    /**
+     * Handles an event
+     *
+     * @param event The event posted
+     */
+    void handle(Object event);
+}
---
+++ src/main/java/br/com/brjdevs/highhacks/eventbus/Listener.java
@@ -0,0 +1,13 @@
+package br.com.brjdevs.highhacks.eventbus;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Marks a method as an event listener
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Listener {}
