4a8c135c47b34276a1880779dccdb81db4e34fbb
Natan <semnomemonmes@gmail.com>
2017-04-14 19:47:33 -0300

    Merge branch 'master' of C:\Users\Natan\Desktop\Java\Mantaro with conflicts.

--- src/main/java/net/kodehawa/mantarobot/modules/SimpleCommandCompat.java
+++ src/main/java/net/kodehawa/mantarobot/modules/SimpleCommandCompat.java
@@ -3,42 +3,42 @@ package net.kodehawa.mantarobot.modules;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 public abstract class SimpleCommandCompat implements SimpleCommand {
-	private final Category category;
-	private final String description;
+    private final Category category;
+    private final String description;
 
-	public SimpleCommandCompat(Category category, String description) {
-		this.category = category;
-		this.description = description;
-	}
+    public SimpleCommandCompat(Category category, String description) {
+        this.category = category;
+        this.description = description;
+    }
 
-	protected abstract void call(String[] args, String name, GuildMessageReceivedEvent event);
+    @Override
+    public CommandPermission permission() {
+        return permissionRequired();
+    }
 
-	@Override
-	public void call(GuildMessageReceivedEvent event, String commandName, String[] args) {
-		call(args, commandName, event);
-	}
+    public CommandPermission permissionRequired() {
+        return CommandPermission.USER;
+    }
 
-	@Override
-	public Category category() {
-		return category;
-	}
+    @Override
+    public String description() {
+        return description;
+    }
 
-	@Override
-	public String description() {
-		return description;
-	}
+    @Override
+    public Category category() {
+        return category;
+    }
 
-	@Override
-	public boolean isHiddenFromHelp() {
-		return false;
-	}
+    @Override
+    public boolean isHiddenFromHelp() {
+        return false;
+    }
 
-	@Override
-	public CommandPermission permission() {
-		return permissionRequired();
-	}
+    @Override
+    public void call(GuildMessageReceivedEvent event, String commandName, String[] args) {
+        call(args, commandName, event);
+    }
 
-	public CommandPermission permissionRequired() {
-		return CommandPermission.USER;
-	}
+    protected abstract void call(String[] args, String name, GuildMessageReceivedEvent event);
 }
