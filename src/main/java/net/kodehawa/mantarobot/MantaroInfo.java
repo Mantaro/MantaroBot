dad1f638583ce73e163d29bfffbb52495e5fa523
Kodehawa <david.alejandro.rubio@gmail.com>
2017-05-18 12:08:21 -0400

    simplify version number

--- src/main/java/net/kodehawa/mantarobot/MantaroInfo.java
+++ src/main/java/net/kodehawa/mantarobot/MantaroInfo.java
@@ -1,7 +1,5 @@
 package net.kodehawa.mantarobot;
 
-import net.dv8tion.jda.core.JDAInfo;
-
 public final class MantaroInfo {
-	public static final String VERSION = "@version@_J" + JDAInfo.VERSION;
+	public static final String VERSION = "@version@";
 }
