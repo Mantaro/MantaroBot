96c063740a7074736d2d73d7ba8cfe28d857c047
natanbc <semnomemonmes@gmail.com>
2017-07-06 16:26:55 -0300

    Kode read what you import kthx

--- src/main/java/net/kodehawa/mantarobot/shard/MantaroShard.java
+++ src/main/java/net/kodehawa/mantarobot/shard/MantaroShard.java
@@ -29,7 +29,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
-import java.sql.Time;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
