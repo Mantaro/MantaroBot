3581eed2df390b9b8c735ffa04994875324d5c83
Kodehawa <david.alejandro.rubio@gmail.com>
2017-08-02 22:55:35 -0400

    Changed a bunch of stuff in the old action commands. Now with more gifs.

--- src/main/java/net/kodehawa/mantarobot/commands/ActionCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/ActionCmds.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.kodehawa.mantarobot.commands.action.ImageActionCmd;
+import net.kodehawa.mantarobot.commands.action.ImageCmd;
 import net.kodehawa.mantarobot.commands.action.TextActionCmd;
 import net.kodehawa.mantarobot.modules.CommandRegistry;
 import net.kodehawa.mantarobot.modules.Module;
@@ -21,13 +22,14 @@ import java.util.stream.Collectors;
 
 @Module
 public class ActionCmds {
-	//TODO API-fy this images
-	final DataManager<List<String>> BITES = new SimpleFileDataManager("assets/mantaro/texts/bites.txt");
-	final DataManager<List<String>> GREETINGS = new SimpleFileDataManager("assets/mantaro/texts/greetings.txt");
-	final DataManager<List<String>> HUGS = new SimpleFileDataManager("assets/mantaro/texts/hugs.txt");
-	final DataManager<List<String>> PATS = new SimpleFileDataManager("assets/mantaro/texts/pats.txt");
-	final DataManager<List<String>> POKES = new SimpleFileDataManager("assets/mantaro/texts/pokes.txt");
-	final DataManager<List<String>> SLAPS = new SimpleFileDataManager("assets/mantaro/texts/slaps.txt");
+
+	//File declarations
+	private final DataManager<List<String>> BITES = new SimpleFileDataManager("assets/mantaro/texts/bites.txt");
+	private final DataManager<List<String>> GREETINGS = new SimpleFileDataManager("assets/mantaro/texts/greetings.txt");
+	private final DataManager<List<String>> HUGS = new SimpleFileDataManager("assets/mantaro/texts/hugs.txt");
+	private final DataManager<List<String>> PATS = new SimpleFileDataManager("assets/mantaro/texts/pats.txt");
+	private final DataManager<List<String>> POKES = new SimpleFileDataManager("assets/mantaro/texts/pokes.txt");
+	private final DataManager<List<String>> SLAPS = new SimpleFileDataManager("assets/mantaro/texts/slaps.txt");
 	private final DataManager<List<String>> BLEACH = new SimpleFileDataManager("assets/mantaro/texts/bleach.txt");
 	private final DataManager<List<String>> HIGHFIVES = new SimpleFileDataManager("assets/mantaro/texts/highfives.txt");
 	private final DataManager<List<String>> KISSES = new SimpleFileDataManager("assets/mantaro/texts/kisses.txt");
@@ -37,6 +39,9 @@ public class ActionCmds {
 	private final DataManager<List<String>> LEWD = new SimpleFileDataManager("assets/mantaro/texts/lewd.txt");
 	private final DataManager<List<String>> FACEDESK = new SimpleFileDataManager("assets/mantaro/texts/facedesk.txt");
 	private final DataManager<List<String>> NUZZLE = new SimpleFileDataManager("assets/mantaro/texts/nuzzle.txt");
+	private final DataManager<List<String>> MEOW = new SimpleFileDataManager("assets/mantaro/texts/meow.txt");
+	private final DataManager<List<String>> BLOODSUCK = new SimpleFileDataManager("assets/mantaro/texts/bloodsuck.txt");
+	private final DataManager<List<String>> NOMS = new SimpleFileDataManager("assets/mantaro/texts/nom.txt");
 
 	@Subscribe
 	public void action(CommandRegistry registry) {
@@ -46,11 +51,8 @@ public class ActionCmds {
 				String noArgs = content.split(" ")[0];
 				TextChannel channel = event.getChannel();
 				switch (noArgs) {
-					case "facedesk":
-						channel.sendMessage("http://puu.sh/rK6E7/0b745e5544.gif").queue();
-						break;
 					case "nom":
-						channel.sendMessage("http://puu.sh/rK7t2/330182c282.gif").queue();
+						channel.sendMessage(CollectionUtils.random(NOMS.get())).queue();
 						break;
 					case "bleach":
 						channel.sendMessage(CollectionUtils.random(BLEACH.get())).queue();
@@ -64,82 +66,11 @@ public class ActionCmds {
 			public MessageEmbed help(GuildMessageReceivedEvent event) {
 				return helpEmbed(event, "Action commands")
 					.addField("Usage", "`~>action bleach` - **Random bleach picture**.\n" +
-						"`~>action facedesk` - **When you really mess up.**\n" +
 						"`~>action nom` - **nom nom**.", false)
 					.setColor(Color.PINK)
 					.build();
 			}
 		});
-	}
-
-	@Subscribe
-	public void bloodsuck(CommandRegistry registry) {
-		registry.register("bloodsuck", new SimpleCommand(Category.ACTION) {
-			@Override
-			protected void call(GuildMessageReceivedEvent event, String content, String[] args) {
-				if (event.getMessage().getMentionedUsers().isEmpty()) {
-					event.getChannel().sendFile(ImageActionCmd.CACHE.getInput("http://imgur.com/ZR8Plmd.png"), "suck.png", null).queue();
-				} else {
-					String bString = event.getMessage().getMentionedUsers().stream().map(User::getName).collect(Collectors.joining(" "));
-					String bs = String.format(EmoteReference.TALKING + "%s has sucked the blood of %s", event.getAuthor().getName(),
-						bString);
-					event.getChannel().sendFile(ImageActionCmd.CACHE.getInput("http://imgur.com/ZR8Plmd.png"), "suck.png",
-						new MessageBuilder().append(bs).build()).queue();
-				}
-			}
-
-			@Override
-			public MessageEmbed help(GuildMessageReceivedEvent event) {
-				return helpEmbed(event, "Bloodsuck")
-					.setDescription("**Sucks the blood of the mentioned members**")
-					.build();
-			}
-		});
-	}
-
-	@Subscribe
-	public void lewd(CommandRegistry registry) {
-		registry.register("lewd", new SimpleCommand(Category.ACTION) {
-			@Override
-			protected void call(GuildMessageReceivedEvent event, String content, String[] args) {
-				String lood = event.getMessage().getMentionedUsers().stream().map(IMentionable::getAsMention).collect(Collectors.joining(" "));
-				event.getChannel().sendFile(ImageActionCmd.CACHE.getInput("http://imgur.com/LJfZYau.png"), "lewd.png"
-					, new MessageBuilder().append(lood).append(" Y-You lewdie!").build()).queue();
-			}
-
-			@Override
-			public MessageEmbed help(GuildMessageReceivedEvent event) {
-				return helpEmbed(event, "Lewd")
-					.setDescription("**L-lewd**").build();
-			}
-		});
-	}
-
-	@Subscribe
-	public void meow(CommandRegistry registry) {
-		registry.register("meow", new SimpleCommand(Category.ACTION) {
-			@Override
-			protected void call(GuildMessageReceivedEvent event, String content, String[] args) {
-				Message receivedMessage = event.getMessage();
-				if (!receivedMessage.getMentionedUsers().isEmpty()) {
-					String mew = event.getMessage().getMentionedUsers().stream().map(User::getName).collect(Collectors.joining(" "));
-					event.getChannel().sendFile(ImageActionCmd.CACHE.getInput("http://imgur.com/yFGHvVR.gif"), "mew.gif",
-						new MessageBuilder().append(EmoteReference.TALKING).append(String.format("%s *is meowing at %s.*", event.getAuthor().getName(), mew)).build()).queue();
-				} else {
-					event.getChannel().sendFile(ImageActionCmd.CACHE.getInput("http://imgur.com/yFGHvVR.gif"), "mew.gif",
-						new MessageBuilder().append(":speech_balloon: Meow.").build()).queue();
-				}
-			}
-
-			@Override
-			public MessageEmbed help(GuildMessageReceivedEvent event) {
-				return helpEmbed(event, "Meow command")
-					.setDescription("**Meow either to a person or the sky**.")
-					.setColor(Color.cyan)
-					.build();
-			}
-		});
-		registry.registerAlias("meow", "mew");
 	}
 
 	@Subscribe
@@ -210,10 +141,25 @@ public class ActionCmds {
 			EmoteReference.MEGA + "%s", TSUNDERE.get()
 		));
 
+		//nuzzle()
 		cr.register("nuzzle", new ImageActionCmd(
 		        "Nuzzle Command", "Nuzzles the specified user.", Color.PINK,
                 "nuzzle.gif", EmoteReference.TALKING + "%s is nuzzling %s", NUZZLE.get(), "*nuzzles you*", true
         ));
-	}
 
+		//bloodsuck()
+		cr.register("bloodsuck", new ImageActionCmd("Bloodsuck command", "Sucks the blood of an user", Color.PINK, "bloodsuck.gif",
+				EmoteReference.TALKING + "%s is sucking the blood of %s!", BLOODSUCK.get(), "J-Just how am I meant to? Oh well.. *sucks your blood*", true));
+
+		//lewd()
+		cr.register("lewd", new ImageCmd("Lewd", "T-Too lewd!", "lewd", LEWD.get(), "Y-You lewdie!"));
+
+		//meow()
+		cr.register("meow", new ImageCmd("Meow", "Meows at the specified user.", "meow", MEOW.get(), "Meow."));
+		cr.registerAlias("meow", "mew");
+
+		//facedesk()
+		cr.register("facedesk", new ImageCmd("Facedesk", "When it's just too much to handle.", "facedesk", FACEDESK.get(),
+				"*facedesks*", true));
+	}
 }
---
+++ src/main/java/net/kodehawa/mantarobot/commands/action/ImageCmd.java
@@ -0,0 +1,75 @@
+package net.kodehawa.mantarobot.commands.action;
+
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.kodehawa.mantarobot.modules.commands.NoArgsCommand;
+import net.kodehawa.mantarobot.modules.commands.base.Category;
+import net.kodehawa.mantarobot.utils.cache.URLCache;
+import net.kodehawa.mantarobot.utils.commands.EmoteReference;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static br.com.brjdevs.java.utils.collections.CollectionUtils.random;
+
+public class ImageCmd extends NoArgsCommand {
+    public static final URLCache CACHE = new URLCache(20);
+
+    private final String desc;
+    private final String imageName;
+    private final List<String> images;
+    private final String name;
+    private final String toSend;
+    private boolean noMentions = false;
+
+    public ImageCmd(String name, String desc, String imageName, List<String> images, String toSend){
+        super(Category.ACTION);
+        this.name = name;
+        this.desc = desc;
+        this.imageName = imageName;
+        this.images = images;
+        this.toSend = toSend;
+    }
+
+    public ImageCmd(String name, String desc, String imageName, List<String> images, String toSend, boolean noMentions){
+        super(Category.ACTION);
+        this.name = name;
+        this.desc = desc;
+        this.imageName = imageName;
+        this.images = images;
+        this.toSend = toSend;
+        this.noMentions = noMentions;
+    }
+
+    @Override
+    protected void call(GuildMessageReceivedEvent event, String content) {
+        String random = random(images);
+        String extension = random.substring(random.lastIndexOf("."));
+        MessageBuilder builder = new MessageBuilder();
+        builder.append(EmoteReference.TALKING);
+
+        if(!noMentions){
+            List<User> users = event.getMessage().getMentionedUsers();
+            String names = "";
+            if(users != null) names = users.stream().map(user -> event.getGuild().getMember(user).getEffectiveName()).collect(Collectors.joining(", "));
+            if(!names.isEmpty()) builder.append("**").append(names).append("**, ");
+        }
+
+        builder.append(toSend);
+        event.getChannel().sendFile(
+                CACHE.getInput(random),
+                imageName + "." + extension,
+                builder.build()
+        ).queue();
+    }
+
+    @Override
+    public MessageEmbed help(GuildMessageReceivedEvent event) {
+        return helpEmbed(event, name)
+                .setDescription(desc)
+                .setColor(event.getMember().getColor())
+                .build();
+    }
+}
