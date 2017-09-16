78662ff867503adb60297f0958d212ae0ee22ed8
Kodehawa <david.alejandro.rubio@gmail.com>
2017-09-15 21:11:33 -0300

    Add some extra spicy stuff.

--- src/main/java/net/kodehawa/mantarobot/commands/PremiumCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/PremiumCmds.java
@@ -17,8 +17,11 @@
 package net.kodehawa.mantarobot.commands;
 
 import com.google.common.eventbus.Subscribe;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.kodehawa.mantarobot.MantaroBot;
 import net.kodehawa.mantarobot.core.CommandRegistry;
 import net.kodehawa.mantarobot.core.modules.Module;
 import net.kodehawa.mantarobot.core.modules.commands.SimpleCommand;
@@ -30,10 +33,12 @@ import net.kodehawa.mantarobot.db.entities.DBUser;
 import net.kodehawa.mantarobot.db.entities.PremiumKey;
 import net.kodehawa.mantarobot.utils.commands.EmoteReference;
 
+import java.util.concurrent.TimeUnit;
+
 import static java.lang.System.currentTimeMillis;
 
 @Module
-public class PremiumCmd {
+public class PremiumCmds {
     @Subscribe
     public void comprevip(CommandRegistry cr){
         cr.register("activatekey", new SimpleCommand(Category.UTILS) {
@@ -122,6 +127,82 @@ public class PremiumCmd {
                         .setDescription("Activates a premium key!\n" +
                                 "Example: `~>activatekey a4e98f07-1a32-4dcc-b53f-c540214d54ec user` or `~>activatekey 550e8400-e29b-41d4-a716-446655440000 guild`\n" +
                                 "No, those aren't valid keys.")
+                        .build();
+            }
+        });
+    }
+
+    @Subscribe
+    public void checkpremium(CommandRegistry cr) {
+        cr.register("vipstatus", new SimpleCommand(Category.INFO) {
+            @Override
+            protected void call(GuildMessageReceivedEvent event, String content, String[] args) {
+                EmbedBuilder embedBuilder = new EmbedBuilder();
+
+                if(args.length == 0) {
+                    DBUser dbUser = MantaroData.db().getUser(event.getAuthor());
+                    if(!dbUser.isPremium()) {
+                        event.getChannel().sendMessage(EmoteReference.ERROR + "You aren't premium :c").queue();
+                        return;
+                    }
+
+                    embedBuilder.setAuthor("Your Premium Status", null, event.getAuthor().getEffectiveAvatarUrl());
+
+                    if(dbUser.getData().getPremiumKey() != null) {
+                        PremiumKey currentKey = MantaroData.db().getPremiumKey(MantaroData.db().getUser(event.getAuthor()).getData().getPremiumKey());
+                        User owner = MantaroBot.getInstance().getUserById(currentKey.getOwner());
+                        if(owner == null)
+                            owner = event.getAuthor();
+
+                        embedBuilder.setDescription("**Premium user! <3**")
+                                .addField("Valid for", currentKey.validFor() + " days", false)
+                                .addField("Key total duration", currentKey.getDurationDays() + " days", false)
+                                .addField("Key owner", owner.getName() + "#" + owner.getDiscriminator(), false);
+                    } else {
+                        embedBuilder.setDescription("**Premium user under the old system! Don't worry tho, you're premium and your benefits" +
+                                " will work properly!**")
+                                .addField("Valid for", TimeUnit.MILLISECONDS.toDays(dbUser.getPremiumUntil() - currentTimeMillis()) + " days more", false);
+                    }
+
+                    embedBuilder.setFooter("Thanks you for your support <3", null);
+                    event.getChannel().sendMessage(embedBuilder.build()).queue();
+                } else {
+                    if(args[0].equals("guild")) {
+                        DBGuild dbGuild = MantaroData.db().getGuild(event.getGuild());
+
+                        embedBuilder.setAuthor(event.getGuild().getName() + "'s Premium Status", null, event.getAuthor().getEffectiveAvatarUrl());
+
+                        if(dbGuild.getData().getPremiumKey() != null) {
+                            PremiumKey currentKey = MantaroData.db().getPremiumKey(MantaroData.db().getUser(event.getAuthor()).getData().getPremiumKey());
+                            User owner = MantaroBot.getInstance().getUserById(currentKey.getOwner());
+                            if(owner == null)
+                                owner = event.getAuthor();
+
+                            embedBuilder.setDescription("**Premium guild! <3**")
+                                    .addField("Valid for", currentKey.validFor() + " days", false)
+                                    .addField("Key total duration", currentKey.getDurationDays() + " days", false)
+                                    .addField("Key owner", owner.getName() + "#" + owner.getDiscriminator(), false);
+                        } else {
+                            embedBuilder.setDescription("**Premium guild under the old system! Don't worry tho, you're premium and your benefits" +
+                                    " will work properly!**")
+                                    .addField("Valid for", TimeUnit.MILLISECONDS.toDays(dbGuild.getPremiumUntil() - currentTimeMillis()) + " days more", false);
+                        }
+
+                        embedBuilder.setFooter("Thanks you for your support <3", null);
+                        event.getChannel().sendMessage(embedBuilder.build()).queue();
+                        return;
+                    }
+
+                    event.getChannel().sendMessage(EmoteReference.ERROR + "Wrong arguments...").queue();
+                }
+            }
+
+            @Override
+            public MessageEmbed help(GuildMessageReceivedEvent event) {
+                return helpEmbed(event, "Check premium status")
+                        .setDescription("**Checks your premium status or the guild status**")
+                        .addField("Arguments", "`~>vipstatus` - Returns your premium key status\n" +
+                                "`~>vipstatus guild` - Return this guild's premium status.", false)
                         .build();
             }
         });
