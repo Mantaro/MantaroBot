7c5d0a5d64f6d704888b9bf9456dcc311998dc66
Natan <natanbc@users.noreply.github.com>
2017-09-25 22:46:16 -0300

    First pass on redis caching

--- src/main/java/net/kodehawa/mantarobot/db/entities/MantaroObj.java
+++ src/main/java/net/kodehawa/mantarobot/db/entities/MantaroObj.java
@@ -17,6 +17,7 @@
 package net.kodehawa.mantarobot.db.entities;
 
 import lombok.Data;
+import net.kodehawa.mantarobot.data.MantaroData;
 import net.kodehawa.mantarobot.db.ManagedObject;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -54,13 +55,11 @@ public class MantaroObj implements ManagedObject {
 
     @Override
     public void delete() {
-        r.table(DB_TABLE).get(getId()).delete().runNoReply(conn());
+        MantaroData.db().delete(this);
     }
 
     @Override
     public void save() {
-        r.table(DB_TABLE).insert(this)
-                .optArg("conflict", "replace")
-                .runNoReply(conn());
+        MantaroData.db().save(this);
     }
 }
