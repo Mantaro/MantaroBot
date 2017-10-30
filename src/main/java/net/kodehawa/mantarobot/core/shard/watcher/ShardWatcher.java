3c0d7a56cfa4b6e2784063b52277a65981eba5d7
Kodehawa <david.alejandro.rubio@gmail.com>
2017-10-30 00:14:51 -0300

    yeah no

--- src/main/java/net/kodehawa/mantarobot/core/shard/watcher/ShardWatcher.java
+++ src/main/java/net/kodehawa/mantarobot/core/shard/watcher/ShardWatcher.java
@@ -65,7 +65,6 @@ public class ShardWatcher implements Runnable {
                         LogUtils.shard("Seems like Megumin struck our castle and we got a horribly high amount of dead shards (" + dead.length + ")\n" +
                                 "This could be just due to them reconnecting though, if nothing appears down there talking about how the shards are rebooting " +
                                 "you might aswell ignore this warning.");
-                        LogUtils.shardSimple("<@155867458203287552> ^");
                     }
 
                     for(int id : dead) {
