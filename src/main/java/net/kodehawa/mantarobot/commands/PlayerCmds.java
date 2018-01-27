796c86e3c7cfddebb777189c74fa011db2aa0d14
Lars <mrlar33@outlook.com>
2018-01-27 23:40:43 +0100

    Fix Helper badge

--- src/main/java/net/kodehawa/mantarobot/commands/PlayerCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/PlayerCmds.java
@@ -204,7 +204,7 @@ public class PlayerCmds {
                             saveAfter = true;
                         if(playerData.getMarketUsed() > 1000 && playerData.addBadgeIfAbsent(Badge.COMPULSIVE_BUYER))
                             saveAfter = true;
-                        if(mhMember != null && mhMember.getRoles().stream().anyMatch(r -> r.getIdLong() == 292803391330058241L) && playerData.addBadgeIfAbsent(Badge.HELPER_2))
+                        if(mhMember != null && mhMember.getRoles().stream().anyMatch(r -> r.getIdLong() == 406920476259123201L) && playerData.addBadgeIfAbsent(Badge.HELPER_2))
                             saveAfter = true;
                         if(mhMember != null && mhMember.getRoles().stream().anyMatch(r -> r.getIdLong() == 290257037072531466L || r.getIdLong() == 290902183300431872L) && playerData.addBadgeIfAbsent(Badge.DONATOR_2))
                             saveAfter = true;
