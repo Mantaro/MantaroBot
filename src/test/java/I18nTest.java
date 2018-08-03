678cce6bf4f2ebf6aa10937ddc80b39c0ff5b86f
Kodehawa <david.alejandro.rubio@gmail.com>
2018-08-02 21:07:59 -0400

    a test

---
+++ src/test/java/I18nTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016-2018 David Alejandro Rubio Escares / Kodehawa
+ *
+ * Mantaro is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * Mantaro is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mantaro.  If not, see http://www.gnu.org/licenses/
+ */
+
+import net.kodehawa.mantarobot.core.modules.commands.i18n.I18nContext;
+import net.kodehawa.mantarobot.db.entities.helpers.GuildData;
+import net.kodehawa.mantarobot.db.entities.helpers.UserData;
+import net.kodehawa.mantarobot.utils.LanguageKeyNotFoundException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class I18nTest {
+    @Test
+    public void testI18n() throws LanguageKeyNotFoundException {
+        //Should default to en_US
+        I18nContext context = new I18nContext(new GuildData(), new UserData());
+        Assert.assertEquals("en_US", context.getContextLanguage());
+
+        String localized = context.get("test.inherited");
+        Assert.assertNotNull(localized);
+        Assert.assertEquals("owo", localized);
+    }
+}
