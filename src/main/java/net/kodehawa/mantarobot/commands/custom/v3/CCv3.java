431ccc8de53f9b5b22c7bb27dd45eab2c6682119
kodehawa <david.alejandro.rubio@gmail.com>
2019-09-04 00:28:44 -0400

    evaluate only if both comparators are present

--- src/main/java/net/kodehawa/mantarobot/commands/custom/v3/CCv3.java
+++ src/main/java/net/kodehawa/mantarobot/commands/custom/v3/CCv3.java
@@ -114,18 +114,18 @@ public class CCv3 {
         });
 
         DEFAULT_OPERATIONS.put("or", (__, args) -> {
-            String input1 = args.get(0).evaluate();
             String c1 = args.get(1).evaluate();
-            String input2 = args.get(2).evaluate();
-
-            String input3 = args.get(3).evaluate();
             String c2 = args.get(4).evaluate();
-            String input4 = args.get(5).evaluate();
 
             BiPredicate<String, String> comparator1 = comparators.get(c1);
             BiPredicate<String, String> comparator2 = comparators.get(c2);
 
             if(comparator1 != null && comparator2 != null) {
+                String input1 = args.get(0).evaluate();
+                String input2 = args.get(2).evaluate();
+                String input3 = args.get(3).evaluate();
+                String input4 = args.get(5).evaluate();
+
                 if(comparator1.test(input1, input2) || comparator2.test(input3, input4)) {
                     return args.get(6).evaluate();
                 }
