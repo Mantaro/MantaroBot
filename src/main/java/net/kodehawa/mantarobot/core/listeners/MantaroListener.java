5290dbe92cb8f419b78adc17d092f4da8771de3d
kodehawa <david.alejandro.rubio@gmail.com>
2020-02-03 14:44:02 -0300

    Don't send a key if the user already has an active key.

--- src/main/java/net/kodehawa/mantarobot/core/listeners/MantaroListener.java
+++ src/main/java/net/kodehawa/mantarobot/core/listeners/MantaroListener.java
@@ -293,9 +293,11 @@ public class MantaroListener implements EventListener {
                 User user = event.getUser();
                 //who...
                 DBUser dbUser = db.getUser(user);
+                PremiumKey currentKey = MantaroData.db().getPremiumKey(dbUser.getData().getPremiumKey());
+
                 if(event.getMember().getRoles().stream().anyMatch(r -> r.getId().equals("290257037072531466"))) {
                     //Thanks lombok for the meme names
-                    if(!dbUser.getData().isHasReceivedFirstKey()) {
+                    if(!dbUser.getData().isHasReceivedFirstKey() && (currentKey == null || currentKey.validFor() < 20)) {
                         //Attempt to open a PM and send a key!
                         user.openPrivateChannel().queue(channel -> {
                             //Sellout message :^)
