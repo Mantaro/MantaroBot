4f34fce1e350793c45d1ef473f6fa4131bf4d5b9
FivePB (Xer) <admin@zeroCVE.com>
2020-02-24 23:32:33 +0100

    Logic instead of loop

--- src/main/java/net/kodehawa/mantarobot/commands/MoneyCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/MoneyCmds.java
@@ -192,8 +192,7 @@ public class MoneyCmds {
                     else
                         returnMessage.add(String.format(languageContext.withRoot("commands","daily.streak.up"), streak));
                     if(currentDailyOffset < 0){
-                        int streakSaversUsed = 1;
-                        for(; currentDailyOffset + streakSaversUsed * DAILY_VALID_PERIOD_MILLIS < 0; streakSaversUsed++);
+                        int streakSaversUsed = -1 * (int) Math.floor((double) currentDailyOffset / (double) DAILY_VALID_PERIOD_MILLIS);
                         authorPlayer.getInventory().process(new ItemStack(Items.MAGIC_WATCH, streakSaversUsed * -1));
                         returnMessage.add(String.format(languageContext.withRoot("commands", "daily.streak.watch_used"), streakSaversUsed, streakSaversUsed + 1, amountStreaksavers - streakSaversUsed));
                     }
