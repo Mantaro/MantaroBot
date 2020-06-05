2157f48a73f5140cf1b789cac922a64171d07228
natanbc <natanbc@users.noreply.github.com>
2020-06-04 22:39:24 -0300

    fix shit

--- src/main/java/net/kodehawa/mantarobot/utils/exporters/DiscordLatencyExports.java
+++ src/main/java/net/kodehawa/mantarobot/utils/exporters/DiscordLatencyExports.java
@@ -6,6 +6,8 @@ import net.kodehawa.mantarobot.MantaroBot;
 import java.util.concurrent.TimeUnit;
 
 public class DiscordLatencyExports {
+    private static final double MILLISECONDS_PER_SECOND = 1000;
+
     private static final Gauge GATEWAY_LATENCY = Gauge.build()
             .name("mantaro_shard_latency")
             .help("Gateway latency in seconds, per shard")
@@ -26,10 +28,11 @@ public class DiscordLatencyExports {
                 var ping = s.getGatewayPing();
                 if(ping >= 0) {
                     GATEWAY_LATENCY.labels(String.valueOf(s.getShardInfo().getShardId()))
-                            .set(ping);
+                            .set(ping / MILLISECONDS_PER_SECOND);
                 }
             });
-            shards.iterator().next().getRestPing().queue(REST_LATENCY::set);
+            shards.iterator().next().getRestPing().queue(ping ->
+                    REST_LATENCY.set(ping / MILLISECONDS_PER_SECOND));
         }, 0, 3, TimeUnit.SECONDS);
     }
 }
