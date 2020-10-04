3ab495e4ea2bd893516702b59e5bd3f8489b626b
kodehawa <david.alejandro.rubio@gmail.com>
2020-10-04 12:33:08 -0300

    Limit roles to 500 instead of the previous value as the command was quite spammy.

--- src/main/java/net/kodehawa/mantarobot/commands/InfoCmds.java
+++ src/main/java/net/kodehawa/mantarobot/commands/InfoCmds.java
@@ -168,7 +168,7 @@ public class InfoCmds {
                         .addField(languageContext.get("commands.serverinfo.created"),
                                 guild.getTimeCreated().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)), false)
                         .addField(String.format(languageContext.get("commands.serverinfo.roles"),
-                                guild.getRoles().size()), StringUtils.limit(roles, 1016), false)
+                                guild.getRoles().size()), StringUtils.limit(roles, 500), false)
                         .setFooter(String.format(languageContext.get("commands.serverinfo.id_show"), guild.getId()), null)
                         .build()
                 );
