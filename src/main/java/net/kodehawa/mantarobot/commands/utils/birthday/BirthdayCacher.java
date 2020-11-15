1331041443c53629e2adce9267d86b3cecacca8c
kodehawa <david.alejandro.rubio@gmail.com>
2020-11-15 19:33:00 -0300

    Fix(birthday): invalidate old cache when re-building the new cache
    Signed-off-by: kodehawa <david.alejandro.rubio@gmail.com>

--- src/main/java/net/kodehawa/mantarobot/commands/utils/birthday/BirthdayCacher.java
+++ src/main/java/net/kodehawa/mantarobot/commands/utils/birthday/BirthdayCacher.java
@@ -19,6 +19,7 @@ package net.kodehawa.mantarobot.commands.utils.birthday;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.rethinkdb.model.OptArgs;
 import com.rethinkdb.utils.Types;
+import net.kodehawa.mantarobot.commands.UtilsCmds;
 import net.kodehawa.mantarobot.data.MantaroData;
 import net.kodehawa.mantarobot.utils.exporters.Metrics;
 import org.slf4j.Logger;
@@ -74,6 +75,8 @@ public class BirthdayCacher {
                 }
 
                 log.debug("-> [CACHE] Birthdays: {}", cachedBirthdays);
+                log.info("Clearing previous guild birthday cache...");
+                UtilsCmds.getGuildBirthdayCache().invalidateAll();
                 isDone = true;
                 log.info("Cached all birthdays!");
             } catch (Exception e) {
