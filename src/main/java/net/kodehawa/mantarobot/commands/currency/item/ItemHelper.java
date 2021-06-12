8d2c410f2d1aeb11ff2ce5238385ddd88c3304f1
Kodehawa <david.alejandro.rubio@gmail.com>
2021-06-12 16:51:22 -0400

    Add(opencrate): lower cooldown

--- src/main/java/net/kodehawa/mantarobot/commands/currency/item/ItemHelper.java
+++ src/main/java/net/kodehawa/mantarobot/commands/currency/item/ItemHelper.java
@@ -54,7 +54,7 @@ public class ItemHelper {
     private static final IncreasingRateLimiter lootCrateRatelimiter = new IncreasingRateLimiter.Builder()
             .limit(1)
             .spamTolerance(2)
-            .cooldown(20, TimeUnit.SECONDS)
+            .cooldown(15, TimeUnit.SECONDS)
             .maxCooldown(2, TimeUnit.MINUTES)
             .randomIncrement(false)
             .pool(MantaroData.getDefaultJedisPool())
