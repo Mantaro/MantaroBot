1d96497dcd00488120b5eafa5871e9e0413f4d8e
Kodehawa <david.alejandro.rubio@gmail.com>
2022-03-10 21:39:10 -0300

    Fix(command): fix sub-command handling.
    We need to make this work like a normal command, the only difference being that we add this to a parent.

--- src/main/java/net/kodehawa/mantarobot/core/command/slash/SlashSubCommand.java
+++ src/main/java/net/kodehawa/mantarobot/core/command/slash/SlashSubCommand.java
@@ -4,8 +4,10 @@ import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 public abstract class SlashSubCommand extends SlashCommand {
     private final SubcommandData data;
-    public SlashSubCommand(SubcommandData data) {
-        this.data = data;
+    public SlashSubCommand() {
+        super(); // We need to init it like a normal command.
+        data = new SubcommandData(super.getName(), super.getDescription());
+        data.addOptions(super.getOptions());
     }
 
     public SubcommandData getData() {
