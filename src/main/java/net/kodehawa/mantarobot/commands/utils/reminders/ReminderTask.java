9501c142da7b59c3ade33998619e254b682ceb0f
Kodehawa <david.alejandro.rubio@gmail.com>
2022-06-03 19:58:58 -0400

    Fix(reminder): fix reminders dying randomly

--- src/main/java/net/kodehawa/mantarobot/commands/utils/reminders/ReminderTask.java
+++ src/main/java/net/kodehawa/mantarobot/commands/utils/reminders/ReminderTask.java
@@ -36,57 +36,65 @@ public class ReminderTask {
 
     public static void handle() {
         log.debug("Checking reminder data...");
-        try (Jedis j = MantaroData.getDefaultJedisPool().getResource()) {
-            Set<String> reminders = j.zrange("zreminder", 0, 14);
-            var bot = MantaroBot.getInstance();
+        try {
+            // The issue with handling this kind of stuff with ScheduledTasks is that we need to catch everything
+            // In case the Redis server is, for some reason, unavailable, or getting the resource fails
+            // This will silently die and kill every further reminder from being fired.
+            // Such, we need two try/catches here, one so the ScheduledTask doesn't die, and one so the loop doesn't break
+            // Java moments.
+            try (Jedis j = MantaroData.getDefaultJedisPool().getResource()) {
+                Set<String> reminders = j.zrange("zreminder", 0, 14);
+                var bot = MantaroBot.getInstance();
 
-            log.debug("Reminder check - remainder is: {}", reminders.size());
+                log.debug("Reminder check - remainder is: {}", reminders.size());
+                for (var rem : reminders) {
+                    try { // If we don't try here regardless, we kill the loop.
+                        var data = new JSONObject(rem);
+                        var fireAt = data.getLong("at");
 
-            for (var rem : reminders) {
-                try {
-                    var data = new JSONObject(rem);
-                    var fireAt = data.getLong("at");
+                        // If the time has passed...
+                        if (System.currentTimeMillis() >= fireAt) {
+                            log.debug("Reminder date has passed, remind accordingly.");
+                            var userId = data.getString("user");
+                            var fullId = data.getString("id") + ":" + userId;
+                            var guildId = data.getString("guild");
+                            var scheduledAt = data.getLong("scheduledAt");
 
-                    // If the time has passed...
-                    if (System.currentTimeMillis() >= fireAt) {
-                        log.debug("Reminder date has passed, remind accordingly.");
-                        var userId = data.getString("user");
-                        var fullId = data.getString("id") + ":" + userId;
-                        var guildId = data.getString("guild");
-                        var scheduledAt = data.getLong("scheduledAt");
+                            // 1 day passed already, assuming it's a stale reminder:
+                            // Done because ReminderTask wasn't working.
+                            if (System.currentTimeMillis() - fireAt > TimeUnit.DAYS.toMillis(1)) {
+                                Reminder.cancel(userId, fullId, Reminder.CancelReason.CANCEL);
+                                return;
+                            }
 
-                        // 1 day passed already, assuming it's a stale reminder:
-                        // Done because ReminderTask wasn't working.
-                        if (System.currentTimeMillis() - fireAt > TimeUnit.DAYS.toMillis(1)) {
-                            Reminder.cancel(userId, fullId, Reminder.CancelReason.CANCEL);
-                            return;
-                        }
-
-                        var reminder = data.getString("reminder"); //The actual reminder data
-                        var guild = bot.getShardManager().getGuildById(guildId);
-                        var scheduledTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(scheduledAt), ZoneId.systemDefault());
-                        bot.getShardManager().retrieveUserById(userId)
-                                .flatMap(User::openPrivateChannel)
-                                .flatMap(privateChannel -> privateChannel
-                                        .sendMessageFormat("""
+                            var reminder = data.getString("reminder"); //The actual reminder data
+                            var guild = bot.getShardManager().getGuildById(guildId);
+                            var scheduledTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(scheduledAt), ZoneId.systemDefault());
+                            bot.getShardManager().retrieveUserById(userId)
+                                    .flatMap(User::openPrivateChannel)
+                                    .flatMap(privateChannel -> privateChannel
+                                            .sendMessageFormat("""
                                                         %s**Reminder!**
                                                         
                                                         You asked me to remind you of: **%s**
                                                         Asked at: <t:%s>%s""",
-                                                EmoteReference.POPPER,
-                                                reminder.trim().isEmpty() ? "something" : reminder, scheduledTime.toEpochSecond(),
-                                                (guild != null ? "\nAsked on: %s".formatted(guild.getName()) : "")
-                                        )
-                                ).queue(success -> {
-                                    log.debug("Reminded {}. Removing from remind database", fullId);
-                                    Reminder.cancel(userId, fullId, Reminder.CancelReason.REMINDED);
-                                }, err -> Reminder.cancel(userId, fullId, Reminder.CancelReason.ERROR_DELIVERING)
-                        );
+                                                    EmoteReference.POPPER,
+                                                    reminder.trim().isEmpty() ? "something" : reminder, scheduledTime.toEpochSecond(),
+                                                    (guild != null ? "\nAsked on: %s".formatted(guild.getName()) : "")
+                                            )
+                                    ).queue(success -> {
+                                                log.debug("Reminded {}. Removing from remind database", fullId);
+                                                Reminder.cancel(userId, fullId, Reminder.CancelReason.REMINDED);
+                                            }, err -> Reminder.cancel(userId, fullId, Reminder.CancelReason.ERROR_DELIVERING)
+                                    );
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
