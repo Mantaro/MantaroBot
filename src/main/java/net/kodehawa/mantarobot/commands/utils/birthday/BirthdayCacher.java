4c9cd3754f0dac5b3f409142091ad4d7c1f8e835
Kodehawa <david.alejandro.rubio@gmail.com>
2023-05-23 11:34:12 -0400

    Fix(birthday): cache doesnt need another executor in an executor

--- src/main/java/net/kodehawa/mantarobot/commands/utils/birthday/BirthdayCacher.java
+++ src/main/java/net/kodehawa/mantarobot/commands/utils/birthday/BirthdayCacher.java
@@ -17,18 +17,14 @@
 
 package net.kodehawa.mantarobot.commands.utils.birthday;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import net.kodehawa.mantarobot.commands.BirthdayCmd;
 import net.kodehawa.mantarobot.data.MantaroData;
 import net.kodehawa.mantarobot.db.entities.MongoUser;
-import net.kodehawa.mantarobot.utils.exporters.Metrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * Caches the birthday date of all users seen on bot startup and adds them to a local ConcurrentHashMap.
@@ -36,54 +32,49 @@ import java.util.concurrent.Executors;
  */
 public class BirthdayCacher {
     private static final Logger log = LoggerFactory.getLogger(BirthdayCacher.class);
-    private final ExecutorService executorService =
-            Executors.newFixedThreadPool(1, new ThreadFactoryBuilder().setNameFormat("Mantaro Birthday Assigner Executor").build());
     private final Map<Long, BirthdayData> cachedBirthdays = new ConcurrentHashMap<>();
     public volatile boolean isDone;
 
     public BirthdayCacher() {
-        Metrics.THREAD_POOL_COLLECTOR.add("birthday-cacher", executorService);
         log.info("Caching birthdays...");
         cache();
     }
 
     public void cache() {
-        executorService.submit(() -> {
-            try {
-                var users = MantaroData.db().dbMantaro().getCollection("users", MongoUser.class).find();
-                cachedBirthdays.clear();
+        try {
+            var users = MantaroData.db().dbMantaro().getCollection("users", MongoUser.class).find();
+            cachedBirthdays.clear();
 
-                for (var r : users) {
-                    try {
-                        var id = Long.parseUnsignedLong(r.getId());
-                        // Why?
-                        if (cachedBirthdays.containsKey(id))
-                            continue;
+            for (var r : users) {
+                try {
+                    var id = Long.parseUnsignedLong(r.getId());
+                    // Why?
+                    if (cachedBirthdays.containsKey(id))
+                        continue;
 
-                        var birthday = r.getBirthday();
-                        if (birthday != null && !birthday.isEmpty()) {
-                            log.debug("-> PROCESS: {}", r);
-                            var bd = birthday.split("-");
-                            cachedBirthdays.put(id, new BirthdayData(birthday, Long.parseLong(bd[0]), Long.parseLong(bd[1])));
-                        }
-                    } catch (Exception e) {
-                        log.error("Error inserting user to birthday cache?", e);
+                    var birthday = r.getBirthday();
+                    if (birthday != null && !birthday.isEmpty()) {
+                        log.debug("-> PROCESS: {}", r);
+                        var bd = birthday.split("-");
+                        cachedBirthdays.put(id, new BirthdayData(birthday, Long.parseLong(bd[0]), Long.parseLong(bd[1])));
                     }
+                } catch (Exception e) {
+                    log.error("Error inserting user to birthday cache?", e);
                 }
-
-                log.debug("-> [CACHE] Birthdays: {}", cachedBirthdays);
-                // Else we just don't have anything to clear (first startup)
-                if (BirthdayCmd.getGuildBirthdayCache().size() > 0) {
-                    log.info("Clearing previous guild birthday cache...");
-                    BirthdayCmd.getGuildBirthdayCache().invalidateAll();
-                }
-
-                isDone = true;
-                log.info("Cached all birthdays. Current size is {}", cachedBirthdays.size());
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        });
+
+            log.debug("-> [CACHE] Birthdays: {}", cachedBirthdays);
+            // Else we just don't have anything to clear (first startup)
+            if (BirthdayCmd.getGuildBirthdayCache().size() > 0) {
+                log.info("Clearing previous guild birthday cache...");
+                BirthdayCmd.getGuildBirthdayCache().invalidateAll();
+            }
+
+            isDone = true;
+            log.info("Cached all birthdays. Current size is {}", cachedBirthdays.size());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Map<Long, BirthdayData> getCachedBirthdays() {
